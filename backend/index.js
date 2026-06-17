4d0ebd82ea8e9543792270b635651e670619a2b5
Indrajit Ghosh <indrajitghosh3666@gmail.com>
2026-06-17 13:44:41 +0530

    setup env based cors

--- backend/index.js
+++ backend/index.js
@@ -7,22 +7,23 @@ import authRouter from "./routes/auth.routes.js";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 
-const app= express();
-const PORT= process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-
-app.use(cors({
-    origin:[ "https://cncplottermachine.onrender.com", "http://localhost:5173" ],
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
     credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}))
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRouter);
-app.use("/api/user",userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(PORT,()=>{
-    connectDb();
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`Server is running on port ${PORT}`);
 });
